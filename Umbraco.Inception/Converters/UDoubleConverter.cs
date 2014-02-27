f72c4626798732ed2a8292adcf223d27f1cd959b
nojaf <florian@nojaf.com>
2014-02-27 12:29:02 +0100

    Added some extra converters

---
+++ Umbraco.Inception/Converters/UDoubleConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Inception.Converters
+{
+    public class UDoubleConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string)) return true;
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            string objectAsString = value as string;
+
+            if (!string.IsNullOrEmpty(objectAsString))
+            {
+                double d;
+                if (double.TryParse(objectAsString, out d))
+                {
+                    return d;
+                }
+            }
+
+            if (value == null) return 0.0d;
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (value == typeof(double))
+            {
+                return value.ToString();
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
---
+++ Umbraco.Inception/Converters/UIntegerConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Inception.Converters
+{
+    public class UIntegerConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, System.Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            string valueAsString = value as string;
+            if (!string.IsNullOrEmpty(valueAsString))
+            {
+                int i;
+                if (int.TryParse(valueAsString, out i))
+                {
+                    return i;
+                }
+            }
+
+            if (value == null) return int.MinValue;
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, System.Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, System.Type destinationType)
+        {
+            if (value == typeof(int))
+            {
+                return value;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
